7b4c9fb5ac66e5ee9a071200594d9f27b28893b3
atlasmac <mckinleyatlas@gmail.com>
2022-10-16 21:44:23 -0600

    add faq collaspe component

---
+++ client/src/components/FaqCollaspe.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import faqs from '../faq.json'
+
+const FaqCollaspe = () => {
+  const faq = faqs.map((el) => {
+    return (
+      <div key={el.question} tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box">
+        <div className="collapse-title text-xl font-medium">
+          {el.question}
+        </div>
+        <div className="collapse-content">
+          <p>{el.answer}</p>
+        </div>
+      </div>
+    )
+  })
+  return (
+    <div className='flex flex-col gap-y-2'>
+      <h1 className='text-center text-2xl font-robotoSlab font-bold'>Frequently Asked Questions</h1>
+      {faq}
+    </div>
+  )
+}
+
+export default FaqCollaspe
--- client/src/pages/FAQ.jsx
+++ client/src/pages/FAQ.jsx
@@ -1,61 +1,12 @@
 import React from 'react'
+import FaqCollaspe from '../components/FaqCollaspe'
 
 const FAQ = () => {
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col lg:flex-row">
         <img src="https://placeimg.com/260/400/arch" className="max-w-sm rounded-lg shadow-2xl" />
-        <div className='flex flex-col gap-y-2'>
-          <h1 className='text-center text-2xl font-robotoSlab font-bold'>Frequently Asked Questions</h1>
-          <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box">
-            <div className="collapse-title text-xl font-medium">
-              What is cubic feet per second (cfs)?
-            </div>
-            <div className="collapse-content">
-              <p>The measurement cubic foot per second (cfs or ft3/s) is the rate of water movement representing a volume of 1 cubic foot passing a given point during 1 second.</p>
-            </div>
-          </div>
-          <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box">
-            <div className="collapse-title text-xl font-medium">
-              Focus me to see content
-            </div>
-            <div className="collapse-content">
-              <p>tabIndex={0} attribute is necessary to make the div focusable</p>
-            </div>
-          </div>
-          <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box">
-            <div className="collapse-title text-xl font-medium">
-              Focus me to see content
-            </div>
-            <div className="collapse-content">
-              <p>tabIndex={0} attribute is necessary to make the div focusable</p>
-            </div>
-          </div>
-          <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box">
-            <div className="collapse-title text-xl font-medium">
-              Focus me to see content
-            </div>
-            <div className="collapse-content">
-              <p>tabIndex={0} attribute is necessary to make the div focusable</p>
-            </div>
-          </div>
-          <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box">
-            <div className="collapse-title text-xl font-medium">
-              Focus me to see content
-            </div>
-            <div className="collapse-content">
-              <p>tabIndex={0} attribute is necessary to make the div focusable</p>
-            </div>
-          </div>
-          <div tabIndex={0} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box">
-            <div className="collapse-title text-xl font-medium">
-              Focus me to see content
-            </div>
-            <div className="collapse-content">
-              <p>tabIndex={0} attribute is necessary to make the div focusable</p>
-            </div>
-          </div>
-        </div>
+        <FaqCollaspe />
       </div>
     </div>
   )
